a974834404de8f6614300adb598b825914c0c300
juneyear21 <juneyear21@gmail.com>
2025-08-27 16:11:29 +0530

    7th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,28 @@
-import React from 'react'
-
-const App = () => {
+import React from 'react';
+const App = () => { 
   const [count,setCount] = React.useState(null);
-  const [intervalId, setIntervalId] = React.useState(null);
-
-  const Start = () =>{
-     if(!intervalId){
-      const id = setInterval(()=>{
-        setCount(new Date().toLocaleTimeString());
-      },1000)
-      setIntervalId(id)
-     }
-  };
-
-  const Stop = ()=>{
-    clearInterval(intervalId);
+   const [intervalId, setIntervalId] = React.useState(null);
+   const Start = () =>{ 
+    if(!intervalId){ 
+      const id = setInterval(()=>{ 
+        setCount(new Date().toLocaleTimeString()); },1000);
+         setIntervalId(id) } }; 
+  const PR = ()=>{ 
+    if(intervalId !== null){
+      clearInterval(intervalId); 
     setIntervalId(null);
+    }else{
+     Start();
   }
-
-  const Reset = () =>{
-    setCount(0);
-    Stop();
-  }
-
-  React.useEffect(()=>{
-    return () =>{
-      clearInterval(intervalId)
-    }
-  },[intervalId])
-
-  return (
-    <div>
-      <h2>{count}</h2>
-      <button onClick={Start}>Start</button>
-      <button onClick={Stop}>Stop</button>
-      <button onClick={Reset}>Reset</button>
-    </div>
-  )
-}
-
+  } 
+  const Reset = () =>{ setCount(0); clearInterval(intervalId); 
+    setIntervalId(null); } 
+  React.useEffect(()=>{ return () =>{ clearInterval(intervalId) } },[intervalId]);
+  return ( 
+  <div> 
+    <h2>{count}</h2> 
+    <button onClick={Start}>Start</button> 
+    <button onClick={PR}>Pause/Resume</button> 
+    <button onClick={Reset}>Reset</button> 
+  </div> ) };
 export default App
